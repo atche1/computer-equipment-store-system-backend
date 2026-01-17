cfe9a94113c87e69a8a43e2e5c3b63022dfcf6c4
atche1 <atche120207@gmail.com>
2026-01-17 20:10:34 +0200

    Initial commit

---
+++ src/main/java/com/school/ppmg/computer_equipment_store_system_api/ComputerEquipmentStoreSystemApiApplication.java
@@ -0,0 +1,13 @@
+package com.school.ppmg.computer_equipment_store_system_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComputerEquipmentStoreSystemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComputerEquipmentStoreSystemApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/school/ppmg/computer_equipment_store_system_api/ComputerEquipmentStoreSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.school.ppmg.computer_equipment_store_system_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComputerEquipmentStoreSystemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
